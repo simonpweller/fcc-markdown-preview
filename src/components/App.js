f983612822c8cf32c58d568d681618d00a5814ee
Simon Weller <s.meierbeck@gmail.com>
2017-09-26 22:41:38 +0200

    tweaking initial value

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import MdEditor from './MdEditor';
 import MdDisplay from './MdDisplay';
 
+import initialValue from '../initialValue';
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      markdown: "__Why not write some markdown?__"
+      markdown: initialValue
     }
   }
 
@@ -20,7 +22,7 @@ class App extends React.Component {
         <h1>Markdown Previewer</h1>
         <div className="row">
           <div className="col-xs-12 col-sm-6">
-            <MdEditor editMarkdown={this.editMarkdown.bind(this)}/>
+            <MdEditor markdown={this.state.markdown} editMarkdown={this.editMarkdown.bind(this)}/>
           </div>
           <div className="col-xs-12 col-sm-6">
             <MdDisplay markdown={this.state.markdown}/>
