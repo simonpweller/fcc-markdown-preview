ac7fe96fe0cf3678c573742d895e5a2538c47207
Simon Weller <s.meierbeck@gmail.com>
2020-05-05 23:48:57 +0200

    convert App to function component

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MdEditor from './MdEditor';
 import MdDisplay from './MdDisplay';
 
 import initialValue from '../initialValue';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      markdown: initialValue
-    }
-  }
+const App = () => {
+    const [markdown, setMarkdown] = useState(initialValue);
 
-  editMarkdown(markdown) {
-    this.setState({markdown});
-  }
-
-  render() {
     return (
-      <div className="container">
-        <h1>Markdown Previewer</h1>
-        <div className="row">
-          <div className="col-xs-12 col-sm-6">
-            <MdEditor markdown={this.state.markdown} editMarkdown={this.editMarkdown.bind(this)}/>
-          </div>
-          <div className="col-xs-12 col-sm-6">
-            <MdDisplay markdown={this.state.markdown}/>
-          </div>
+        <div className="container">
+            <h1>Markdown Previewer</h1>
+            <div className="row">
+                <div className="col-xs-12 col-sm-6">
+                    <MdEditor markdown={markdown} editMarkdown={setMarkdown}/>
+                </div>
+                <div className="col-xs-12 col-sm-6">
+                    <MdDisplay markdown={markdown}/>
+                </div>
+            </div>
         </div>
-      </div>
     );
-  }
-}
+};
 
 export default App;
