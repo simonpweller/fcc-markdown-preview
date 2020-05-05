c3d8bd8087d95b68c9654564f79a6b09e6543939
Simon Weller <s.meierbeck@gmail.com>
2020-05-05 23:42:04 +0200

    update dependencies, remove build folder from git, clean up cruft

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
